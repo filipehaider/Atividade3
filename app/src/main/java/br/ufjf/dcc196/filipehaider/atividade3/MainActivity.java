5818c47ecfb28cdf9740d1401b9ab2256a4c8ffa
Filipe Haider <filipehaider@ice.ufjf.br>
2020-10-18 12:03:41 -0300

    Adiciona modelagem do problema e eventos dos botões

--- app/src/main/java/br/ufjf/dcc196/filipehaider/atividade3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/filipehaider/atividade3/MainActivity.java
@@ -3,12 +3,93 @@ package br.ufjf.dcc196.filipehaider.atividade3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public enum Jogada{
+        PEDRA(0), PAPEL(1), TESOURA(2), LAGARTO(3), SPOCK(4);
+
+        private final int valor;
+        Jogada(int valor){
+           this.valor = valor;
+       }
+    }
+    public enum Resultado{
+        DERROTA(-1), EMPATE(0), VITORIA(1);
+
+        private final int valor;
+        Resultado(int valor){
+            this.valor = valor;
+        }
+    }
+
+    public static final Resultado TABELA[][] = {
+        {Resultado.EMPATE, Resultado.DERROTA, Resultado.VITORIA, Resultado.VITORIA, Resultado.DERROTA},
+        {Resultado.VITORIA, Resultado.EMPATE, Resultado.DERROTA, Resultado.DERROTA, Resultado.VITORIA},
+        {Resultado.DERROTA, Resultado.VITORIA, Resultado.EMPATE, Resultado.VITORIA, Resultado.DERROTA},
+        {Resultado.DERROTA, Resultado.VITORIA, Resultado.DERROTA, Resultado.EMPATE, Resultado.VITORIA},
+        {Resultado.VITORIA, Resultado.DERROTA, Resultado.VITORIA, Resultado.DERROTA, Resultado.EMPATE},
+
+    };
+
+
+
+    private Integer pontosCPU = 0;
+    private Integer pontosHumano = 0;
+
+    private Button buttonPedra;
+    private Button buttonPapel;
+    private Button buttonTesoura;
+    private Button buttonLagarto;
+    private Button buttonSpock;
+
+    private ProgressBar progressBarCPU;
+    private ProgressBar progressBarHumano;
+    private TextView textViewStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Componentes de entrada
+        buttonPedra = findViewById(R.id.buttonPedra);
+        buttonPapel = findViewById(R.id.buttonPapel);
+        buttonTesoura = findViewById(R.id.buttonTesoura);
+        buttonLagarto = findViewById(R.id.buttonLagarto);
+        buttonSpock = findViewById(R.id.buttonSpock);
+
+        //Componentes de saída
+        progressBarCPU = findViewById(R.id.progressBarCPU);
+        progressBarHumano = findViewById(R.id.progressBarHumano);
+        textViewStatus = findViewById(R.id.textViewStatus);
     }
+
+    public void buttonPedraClick(View view){
+        rodada(Jogada.PEDRA);
+    }
+    public void buttonPapelClick(View view){
+        rodada(Jogada.PAPEL);
+    }
+    public void buttonTesouraClick(View view){
+        rodada(Jogada.TESOURA);
+    }
+    public void buttonLagartoClick(View view){
+        rodada(Jogada.LAGARTO);
+    }
+    public void buttonSpockClick(View view){
+        rodada(Jogada.SPOCK);
+    }
+
+    public void rodada(Jogada jogada){
+
+
+    }
+
+
 }
+
+
