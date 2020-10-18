37cf82d364ad39dd5be87f464c3458114dba89eb
filipehaider <ovitiguf@gmail.com>
2020-10-18 16:32:41 -0300

    Adiciona reinicio de torneio via dialogo

--- app/src/main/java/br/ufjf/dcc196/filipehaider/atividade3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/filipehaider/atividade3/MainActivity.java
@@ -1,7 +1,9 @@
 package br.ufjf.dcc196.filipehaider.atividade3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -132,6 +134,24 @@ public class MainActivity extends AppCompatActivity {
     private void iniciaTorneio() {
         pontosCPU = 0;
         pontosHumano = 0;
+    }
+
+    public void setTextViewStatusClick(View view){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Reiniciar o torneio");
+        dialogBuilder.setMessage("Deseja reiniciar o torneio zerando o estado atual?");
+        dialogBuilder.setPositiveButton("Reiniciar?",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        iniciaTorneio();
+                        atualizaStatus();
+                    }
+                }
+        );
+        dialogBuilder.create();
+        dialogBuilder.show();
+
     }
 
 }
